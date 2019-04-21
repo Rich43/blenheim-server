0fe584e0594c3aec04eb966c3aab3e3c00804f70
Richie Ward <RichieS@GMail.com>
2019-04-21 15:23:51 +0100

    Add initial files

---
+++ server/blenheim/__init__.py
@@ -0,0 +1,9 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+app = Starlette(debug=True)
+
+
+@app.route('/')
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
---
+++ server/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name="blenheim",
+    version="0.1",
+    author="Richard Ward",
+    author_email="richies@gmail.com",
+    description="A dns server managed through a simple web interface",
+    license="BSD",
+    keywords="dns server",
+    url="http://pynguins.com",
+    packages=find_packages(),
+    install_requires=['starlette', 'uvicorn', 'jinja2'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
