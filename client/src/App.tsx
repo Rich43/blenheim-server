5a9a148bdf00524d8c68a117e37574bc87c03497
Richie Ward <RichieS@GMail.com>
2019-06-12 20:07:20 +0100

    Add graphql support to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Login } from "./components/Login";
+import { client } from "./graphQL";
+import ApolloProvider from "react-apollo/ApolloProvider";
 
 const App: React.FC = () => {
-  return (<Login />);
+    return (
+        <ApolloProvider client={client}>
+            <Login />
+        </ApolloProvider>
+    );
 }
 
 export default App;
