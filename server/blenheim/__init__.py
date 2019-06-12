c13151a19c544c3e11e945f3259a323bfdda4f1d
Richie Ward <RichieS@GMail.com>
2019-06-12 20:06:31 +0100

    Allow cross origin requests.

--- server/blenheim/__init__.py
+++ server/blenheim/__init__.py
@@ -2,11 +2,13 @@ import graphene
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.applications import Starlette
 from starlette.graphql import GraphQLApp
+from starlette.middleware.cors import CORSMiddleware
 
 from blenheim.schema.schema import Query, Mutations
 from blenheim.schema.settings.settings import SettingsMutations
 
 app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'])
 # noinspection PyTypeChecker
 app.add_route(
     '/',
