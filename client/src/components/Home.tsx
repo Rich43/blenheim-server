e1b84e6ad7a614a2e0d54a6c8e671d27fdbedb97
Richie Ward <RichieS@GMail.com>
2019-07-19 15:15:23 +0100

    Use graphql call to get list of domains for dashboard.

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -6,8 +6,27 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import gql from 'graphql-tag';
+import Query from 'react-apollo/Query';
+import { StoreProvider } from '../StoreProvider';
+import { Token, TokenVariables } from './types/Token';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 export const Home: React.FC = (): JSX.Element => {
+    const QUERY = gql`
+        query Token($token: String!) {
+            authentication {
+                token(token: $token)
+            }
+            settings {
+                domains {
+                    name
+                }
+            }
+        }
+    `;
+    const store = useContext(StoreProvider);
     return (
         <Container component='main'>
             <Grid container spacing={2}>
@@ -15,7 +34,23 @@ export const Home: React.FC = (): JSX.Element => {
                     <Card>
                         <CardHeader title='Domains' />
                         <CardContent>
-                            List of domains...
+                            <List>
+                                <Query<Token, TokenVariables> query={QUERY} variables={{ token: store.token }}>
+                                    {({ loading, error, data }) => {
+                                        const result: JSX.Element[] = [];
+                                        let count = 0;
+                                        if (data && data.settings && data.settings.domains) {
+                                            for (const domain of data.settings.domains) {
+                                                if (domain) {
+                                                    result.push((<ListItem key={count}>{domain.name}</ListItem>));
+                                                    count++;
+                                                }
+                                            }
+                                        }
+                                        return result;
+                                    }}
+                                </Query>
+                            </List>
                         </CardContent>
                         <CardActions>
                             <Button size='small'>Configure domains</Button>
---
+++ client/src/components/types/Token.ts
@@ -0,0 +1,31 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Token
+// ====================================================
+
+export interface Token_authentication {
+  __typename: "Authentication";
+  token: string | null;
+}
+
+export interface Token_settings_domains {
+  __typename: "Domain";
+  name: string | null;
+}
+
+export interface Token_settings {
+  __typename: "Settings";
+  domains: (Token_settings_domains | null)[] | null;
+}
+
+export interface Token {
+  authentication: Token_authentication | null;
+  settings: Token_settings | null;
+}
+
+export interface TokenVariables {
+  token: string;
+}
