af596a9ba67f10841ffb333160089d2b1ee9f103
Richie Ward <RichieS@GMail.com>
2019-09-01 09:51:58 +0100

    Add 'add' dialog

--- client/src/components/pages/DomainsList.tsx
+++ client/src/components/pages/DomainsList.tsx
@@ -1,5 +1,18 @@
 import React, { FunctionComponent, useState } from 'react';
-import { Button, Collapse, IconButton, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+    Button,
+    Collapse,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    TextField
+} from '@material-ui/core';
 import { AddCircle, ExpandLess, ExpandMore } from '@material-ui/icons';
 import { DomainsListProps } from '../interfaces';
 
@@ -7,11 +20,13 @@ export const DomainsList: FunctionComponent<DomainsListProps> = (props) => {
     let innerCount = 0;
     const name = props.row ? props.row.name ? props.row.name : '' : '';
     const [open, setOpen] = useState<boolean>(false);
+    const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
+
     return (
         <>
             <ListItem key={props.count}>
                 <ListItemText><Button onClick={() => setOpen(!open)}>{name}</Button></ListItemText>
-                <IconButton><AddCircle /></IconButton>
+                <IconButton onClick={() => setDialogOpen(true)}><AddCircle /></IconButton>
                 <IconButton onClick={() => setOpen(!open)}>{open ? <ExpandLess /> : <ExpandMore />}</IconButton>
             </ListItem>
 
@@ -27,6 +42,30 @@ export const DomainsList: FunctionComponent<DomainsListProps> = (props) => {
                     }
                 </List>
             </Collapse>
+
+            <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+                <DialogTitle>Add Subdomain</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        todo
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin='dense'
+                        id='subdomain'
+                        label='Subdomain:'
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setDialogOpen(false)} color='primary'>
+                        Cancel
+                    </Button>
+                    <Button onClick={() => setDialogOpen(false)} color='primary'>
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
