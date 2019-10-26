e0738343ed57bb1f2ee636174f0d17b93645c647
Richie Ward <RichieS@GMail.com>
2019-10-26 15:55:54 +0100

    Add delete sub domain mutation

--- client/src/components/pages/SubDomainListItem.tsx
+++ client/src/components/pages/SubDomainListItem.tsx
@@ -1,7 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useContext } from "react";
 import { IconButton, ListItem, ListItemText } from "@material-ui/core";
 import { Edit, Remove } from "@material-ui/icons";
 import { UpdateSubDomainDialog } from "../dialogs/custom/UpdateSubDomainDialog";
+import { useDeleteSubDomainMutation } from "../queries/DeleteSubDomainMutation";
+import { StoreProvider } from "../../StoreProvider";
 
 export const SubDomainListItem: FunctionComponent<{
     count: number,
@@ -9,12 +11,23 @@ export const SubDomainListItem: FunctionComponent<{
     subdomain: string
 }> = ({count, domain, subdomain}) => {
     const [dialogOpen, setDialogOpen] = React.useState<boolean>(false);
+    const [deleteSubDomain] = useDeleteSubDomainMutation();
+    const store = useContext(StoreProvider);
+
     return (
         <>
             <ListItem key={`innerLi${count}`}>
                 <ListItemText key={`innerLit${count}`}>{subdomain}</ListItemText>
                 <IconButton onClick={() => setDialogOpen(true)}><Edit/></IconButton>
-                <IconButton><Remove/></IconButton>
+                <IconButton onClick={() => {
+                    deleteSubDomain({
+                        variables: {
+                            token: store.token,
+                            id: domain,
+                            index: count - 1
+                        }
+                    }).then()
+                }}><Remove/></IconButton>
             </ListItem>
             <UpdateSubDomainDialog domainName={domain} index={count - 1} dialogOpen={dialogOpen} onClose={() => setDialogOpen(false)}/>
         </>
