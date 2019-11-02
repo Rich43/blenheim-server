422841c8ed99adecbed5e91f5fd8c82c460bdbb8
Richie Ward <RichieS@GMail.com>
2019-11-02 20:12:46 +0000

    Parameterize remaining strings in MutableList

--- client/src/components/MutableList.tsx
+++ client/src/components/MutableList.tsx
@@ -13,8 +13,12 @@ import React, { FunctionComponent } from 'react';
 
 export const MutableList: FunctionComponent<{
     subheaderText: string;
+    placeholderText: string;
+    listItems: string[];
 }> = ({
-    subheaderText
+    subheaderText,
+    placeholderText,
+    listItems
 }) => {
     return <>
         <List subheader={
@@ -22,18 +26,25 @@ export const MutableList: FunctionComponent<{
                 {subheaderText}
             </ListSubheader>
         }>
-            <ListItem>
-                <ListItemText primary='Single-line item' />
-                <ListItemSecondaryAction>
-                    <IconButton edge='end' aria-label='delete'>
-                        <DeleteIcon />
-                    </IconButton>
-                </ListItemSecondaryAction>
-            </ListItem>
+            {
+                listItems.map(item => {
+                    return (
+                        <ListItem>
+                            <ListItemText primary={item} />
+                            <ListItemSecondaryAction>
+                                <IconButton edge='end' aria-label='delete'>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    );
+                })
+            }
+        }
         </List>
         <Box p={2}>
             <TextField
-                placeholder='Enter a new default subdomain'
+                placeholder={placeholderText}
                 fullWidth
             />
         </Box>
--- client/src/components/pages/Settings.tsx
+++ client/src/components/pages/Settings.tsx
@@ -8,7 +8,11 @@ export const Settings: FunctionComponent = () => {
         <form>
             <Container maxWidth='sm'>
                 <Paper>
-                    <MutableList subheaderText='Default subdomains' />
+                    <MutableList
+                        subheaderText='Default subdomains'
+                        placeholderText='Enter a new default subdomain'
+                        listItems={['Single-line item']}
+                    />
                 </Paper>
             </Container>
         </form>
