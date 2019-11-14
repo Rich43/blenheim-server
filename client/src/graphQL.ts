8d862354cd719677dc60339f99ccf839a1abfdcd
Richie Ward <RichieS@GMail.com>
2019-11-14 20:16:42 +0000

    Make the URL a bit more flexible

--- client/src/graphQL.ts
+++ client/src/graphQL.ts
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-    uri: "http://localhost:8000"
+    uri: `${window.location.protocol}//${window.location.host}:8000`
 });
