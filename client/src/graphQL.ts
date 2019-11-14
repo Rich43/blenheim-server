1c2d0a6c47c719594048ab833428baffeabf811f
Richie Ward <RichieS@GMail.com>
2019-11-14 20:31:47 +0000

    Fix typo

--- client/src/graphQL.ts
+++ client/src/graphQL.ts
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
-    uri: `${window.location.protocol}//${window.location.host}:8000`
+    uri: `${window.location.protocol}//${window.location.hostname}:8000`
 });
