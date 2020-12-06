6a7c5f302b7733a78e7908713f8ce49b018c1cfb
Richie Ward <RichieS@GMail.com>
2020-12-06 16:25:23 +0000

    Cleanup Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,39 +9,17 @@ RUN cp -rfv /etc/bind/* .
 
 WORKDIR /app/server
 
-COPY server .
-
-RUN rm -rfv config
-RUN rm -rfv venv
-RUN rm -rfv .idea
-
-RUN python setup.py bdist_wheel
+COPY dist .
 
 WORKDIR /app/server/dist
 
 RUN find -maxdepth 1 -type f -name '*.whl' -exec pip install {} \;
-
-WORKDIR /app/client
-
-COPY client .
-
-RUN rm -rfv node_modules
-RUN rm -rfv .idea
-
-RUN apt install -y npm
-RUN npm install -g npm@latest
-RUN npm install -g n
-
-RUN n stable
-
-RUN npm i
 
 WORKDIR /app
 
 COPY docker.sh .
 
 EXPOSE 8000/tcp
-EXPOSE 3000/tcp
 EXPOSE 53/tcp
 EXPOSE 53/udp
 
--- docker.sh
+++ docker.sh
@@ -19,34 +19,13 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-cd /app/client || exit
-
-# Substitute server uri
-if  [[ -v BLENHEIM_URI ]]; then
-cd src || exit
-echo "$BLENHEIM_URI" | sed -e 's:\::\\\::g' > uri
-sed -e "s:\${window.location.protocol}//\${window.location.hostname}\:8000:`cat uri`:g" graphQL.ts > graphQLtemp.ts
-mv graphQLtemp.ts graphQL.ts
-cd ..
-fi
-
-# Start blenheim client
-npm start &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start blenheim client: $status"
-  exit $status
-fi
-
 while sleep 10; do
   ps aux | grep named | grep -q -v grep
   BIND_STATUS=$?
   ps aux | grep uvicorn | grep -q -v grep
   BLENHEIM_SERVER_STATUS=$?
-  ps aux | grep npm | grep -q -v grep
-  BLENHEIM_CLIENT_STATUS=$?
 
-  if [ $BIND_STATUS -ne 0 -o $BLENHEIM_SERVER_STATUS -ne 0 -o $BLENHEIM_CLIENT_STATUS -ne 0 ]; then
+  if [ $BIND_STATUS -ne 0 -o $BLENHEIM_SERVER_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit 1
   fi
