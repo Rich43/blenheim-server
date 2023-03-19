eebc4b10444368cd45a9e3a884a52e90cd2e0747
Richard Ward <RichieS@GMail.com>
2023-03-19 17:17:56 +0000

    Simplify Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
 FROM python:latest
 
-RUN apt update
-RUN apt install -y bind9
-
-WORKDIR /app/bind_backup
-
-RUN cp -rfv /etc/bind/* .
-
 WORKDIR /app/server
 
 COPY blenheim_server-0.1-py3-none-any.whl .
@@ -18,8 +11,6 @@ WORKDIR /app
 COPY docker.sh .
 
 EXPOSE 8000/tcp
-EXPOSE 53/tcp
-EXPOSE 53/udp
 
 ENTRYPOINT [ "./docker.sh" ]
 CMD [ ]
--- docker.sh
+++ docker.sh
@@ -1,32 +1,3 @@
 #!/usr/bin/env bash
-# Restore bind backup
-cp -nv /app/bind_backup/* /etc/bind/
-
-# Start bind
-/etc/init.d/bind9 start
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start bind9: $status"
-  exit $status
-fi
-
-# Start blenheim server
 cd /app/server || exit
-uvicorn --host 0.0.0.0 blenheim:app &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start blenheim server: $status"
-  exit $status
-fi
-
-while sleep 10; do
-  ps aux | grep named | grep -q -v grep
-  BIND_STATUS=$?
-  ps aux | grep uvicorn | grep -q -v grep
-  BLENHEIM_SERVER_STATUS=$?
-
-  if [ $BIND_STATUS -ne 0 -o $BLENHEIM_SERVER_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
-done
+uvicorn --host 0.0.0.0 blenheim:app
